8a7f2d88329dcad07745842f7bfd0ac0553dd7b0
G0tem <bindirin@yahoo.com>
2024-11-26 12:35:47 +0500

    add logic insurance, save db, result

--- backend/repositories/InsuranceRepositories.py
+++ backend/repositories/InsuranceRepositories.py
@@ -1,10 +1,53 @@
 from schemas.InsuranceSchemas import InsuranceSchema
 from sqlalchemy.ext.asyncio import AsyncSession
 from models.InsuranceModel import Insurance
+from models.TariffModel import Tariff
+from sqlalchemy import select
+from datetime import datetime
+from decimal import Decimal, ROUND_HALF_UP
+from sqlalchemy import insert
+from datetime import date
 
 
 class InsuranceRepositories:
     """Class for insurance repositories."""
+
     @staticmethod
     async def post_insurance(insurance: InsuranceSchema, session: AsyncSession) -> None:
-        return insurance
+        rate = await InsuranceRepositories.get_rate(insurance, session)
+        total_cost = insurance.declared_cost * Decimal(str(rate))
+        total_cost = total_cost.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+
+        await InsuranceRepositories.save_insurance_result(insurance, total_cost, session)
+        return {"стоимость страхования": total_cost}
+
+    @staticmethod    
+    async def get_rate(insurance: InsuranceSchema, session: AsyncSession) -> float:
+        insurance_date = datetime.strptime(insurance.insurance_date, "%Y-%m-%d").date()
+        query = select(Tariff).where(
+            Tariff.tariff_date == insurance_date,
+            Tariff.cargo_type == insurance.cargo_type
+        )
+        result = await session.execute(query)
+        tariff_model = result.scalars().first()
+
+        if tariff_model:
+            return tariff_model.rate
+        else:
+            raise Exception("Tariff not found for the provided insurance details")
+
+    @staticmethod
+    async def save_insurance_result(insurance: InsuranceSchema, total_cost: Decimal, session: AsyncSession) -> None:
+        insurance_date = date.fromisoformat(insurance.insurance_date)
+
+        insurance_data = {
+            "insurance_date": insurance_date,
+            "cargo_type": insurance.cargo_type,
+            "declared_cost": insurance.declared_cost,
+            "insurance_cost": total_cost
+        }
+
+        stmt = insert(Insurance).values(insurance_data)
+
+        await session.execute(stmt)
+        await session.commit()
--- backend/repositories/TariffRepositories.py
+++ backend/repositories/TariffRepositories.py
@@ -6,32 +6,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from datetime import datetime
 
 
-"""   
-{
-    "2020-06-01": [
-        {
-            "cargo_type": "Glass",
-            "rate": "0.04"
-        },
-        {
-            "cargo_type": "Other",
-            "rate": "0.01"
-        }
-    ],
-    "2020-07-01": [
-        {
-            "cargo_type": "Glass",
-            "rate": "0.035"
-        },
-        {
-            "cargo_type": "Other",
-            "rate": "0.015"
-        }
-    ]
-}
-""" 
-
-
 class TariffRepositories:
     """Class for tariff repositories."""
     @staticmethod
